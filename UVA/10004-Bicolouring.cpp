831846046841684e38af22f3dc829268cc6fd233
David <zeroxk123@gmail.com>
2013-01-19 00:58:34 +0100

    Finished UVA 10004-BiColouring

--- UVA/10004-Bicolouring.cpp
+++ UVA/10004-Bicolouring.cpp
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <vector>
-#define MAXNODES 20;
-int[MAXNODES] visited;
-int[MAXNODES] coloured;
-bool dfs(vector<vector<int>> graph, int node) {
+const int MAXNODES = 201;
+int visited [MAXNODES];
+int coloured [MAXNODES];
+bool dfs(std::vector<std::vector<int> > graph, int node) {
 
 	visited[node] = 1;
 	for(int i=0;i<graph[node].size();i++) {
 		int neighbour;
 		neighbour = graph[node][i];
-		if(visited[neighbour] == 0) {
+		if(visited[neighbour] == -1) {
+			coloured[neighbour] = (coloured[node]+1)%2;
 			dfs(graph,neighbour);
-		}else if(coloured[neighbour] == coloured[node]) {
-			return false;
 		}
+
+		if(coloured[neighbour] == coloured[node]) {
+			return false;
+		} 
 	}
 	return true;
 
@@ -22,28 +25,43 @@ bool dfs(vector<vector<int>> graph, int node) {
 int main(void) {
 
 	while(1) {
-	vector<vector<int>> graph;
+	std::vector<std::vector<int> > graph;
 	int nodes,edges;
-	scanf("%d\n%d",&nodes,&edges);
+	scanf("%d",&nodes);
 	if(nodes == 0) break;
+	scanf("%d",&edges);
 
-	graph = vector<vector<int>>(nodes);
+	graph = std::vector<std::vector<int> >(nodes);
 	for(int i=0;i<edges;i++) {
 
 		int a,b;
 		scanf("%d %d",&a,&b);
-		graph[a].push_back(b);
-		graph[b].push_back(a);
+		
+		if(a < nodes && 0 <= a && b < nodes && 0 <= b) {
+			graph[a].push_back(b);
+			graph[b].push_back(a);
 
+		}
+		
 	}
-	bool isBicolourable = dfs(graph, 0);
-	if(isBicolourable) {
-		printf("BICOLOURABLE");
-	}else {
-		printf("NOT BICOLOURABLE");
+
+	for(int i=0;i<nodes;i++) {
+		coloured[i] = -1;
+		visited[i] = -1;
 	}
 	
+	int startNode = graph.front().front();
+	coloured[startNode] = 0;
+	bool isBicolourable = dfs(graph, startNode);
+	if(isBicolourable) {
+		printf("BICOLORABLE.\n");
+	}else{
+		printf("NOT BICOLORABLE.\n");
 	}
+	
+	
+	}
+	
 	return 0;
 
 }

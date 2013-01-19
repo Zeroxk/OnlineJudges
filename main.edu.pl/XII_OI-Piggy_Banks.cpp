21ab7eb30a1b76c98b42792481c687e78790422a
David <zeroxk123@gmail.com>
2013-01-19 06:59:14 +0100

    Input-output for PiggyBanks

---
+++ main.edu.pl/XII_OI-Piggy_Banks.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int main(void) {
+
+    int piggyBanks;
+    scanf("%d",&piggyBanks);
+    int keys[piggyBanks];
+    for(int i=0;i<piggyBanks;i++) {
+    
+        scanf("%d",piggyBanks[0]);
+    
+    }
+
+    return 0;
+
+}

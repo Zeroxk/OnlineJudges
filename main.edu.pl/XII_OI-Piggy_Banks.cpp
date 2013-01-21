ccb5c2d280fe9cd5a16283d90a786a465261a0f3
David <zeroxk123@gmail.com>
2013-01-21 02:33:17 +0100

    Updated PiggyBanks

--- main.edu.pl/XII_OI-Piggy_Banks.cpp
+++ main.edu.pl/XII_OI-Piggy_Banks.cpp
@@ -1,15 +1,68 @@
 #include <cstdio>
+#include <vector>
+const int MAXNODES = 1000000;
+int visited [MAXNODES];
+
+void dfs(std::vector<std::vector<int> >, int);
+int findAllComponents(std::vector<std::vector<int> >);
+
+int findAllComponents(std::vector<std::vector<int> > graph) {
+
+    int components;
+    components = 0;
+    for(int i=0;i<graph.size();i++) {
+    
+        if(visited[i] == -1) {
+        
+            components++;
+            dfs(graph,i);
+
+        }
+
+    }
+
+    return components;
+
+}
+
+void dfs(std::vector<std::vector<int> > graph, int node) {
+
+    visited[node] = 1;
+    for(int i=0; i<graph[node].size(); i++) {
+    
+        int neighbour;
+        neighbour = graph[node][i];
+        if(visited[neighbour] == -1) {
+            dfs(graph,neighbour);
+        }
+    
+    }
+
+}
 
 int main(void) {
 
     int piggyBanks;
     scanf("%d",&piggyBanks);
-    int keys[piggyBanks];
+    std::vector<std::vector<int> > graph;
+    graph = std::vector<std::vector<int> >(piggyBanks);
+
     for(int i=0;i<piggyBanks;i++) {
     
-        scanf("%d",piggyBanks[0]);
+        int key;
+        scanf("%d",&key);
+        graph[i].push_back(key);
     
     }
+    
+    for(int i=0; i<piggyBanks; i++) {
+    
+        visited[i] = -1;
+    
+    }
+
+    int components = findAllComponents(graph);
+    printf("%d",components);
 
     return 0;
 

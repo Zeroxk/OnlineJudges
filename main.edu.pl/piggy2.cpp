25ea84be715f9ebc6443551e7e2c4e8064c897dd
dhu009 <dhu009@student.uib.no>
2013-02-12 16:02:05 +0100

    Alt version of Piggybanks

---
+++ main.edu.pl/piggy2.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <vector>
+
+int components;
+
+int main(void) {
+
+    int piggyBanks;
+    scanf("%d",&piggyBanks);
+    components = piggyBanks;
+    std::vector<int> graph = std::vector<int>();
+
+    for(int i=0;i<piggyBanks;i++) {
+        graph.push_back(i);
+    }
+
+    for(int i=0;i<piggyBanks;i++) {
+    
+        int key;
+        scanf("%d",&key);
+        key--;
+
+        if(key != i) {
+        
+            int a = i;
+            int b = key;
+            while(graph[a] != a) {
+                a = graph[a];
+            }
+            while(graph[b] != b) {
+                b = graph[b];
+            }
+            if(a != b) {
+                graph[i] = b;
+                components--;
+            }
+        }
+    }
+
+    printf("Number of components: %d\n",components);
+
+    return 0;
+
+}

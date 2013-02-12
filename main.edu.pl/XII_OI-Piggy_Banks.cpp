6b337c067cc798e06fd677668356df31522798e4
David <zeroxk123@gmail.com>
2013-02-12 01:23:16 +0100

    UPdated Piggy Banks

--- main.edu.pl/XII_OI-Piggy_Banks.cpp
+++ main.edu.pl/XII_OI-Piggy_Banks.cpp
@@ -1,21 +1,22 @@
 #include <cstdio>
 #include <vector>
-const int MAXNODES = 1000000;
+int components;
 
-typedef struct {
+typedef struct Node Node;
+struct Node {
 
     int elem;
     Node *parent;
 
 
-}Node;
+};
 
-Node find(Node* n) {
+Node* find(Node* n) {
 
-    if(&n.parent == n) {
+    if(n->parent == n) {
         return n;
     }else {
-        return find(&n.parent);
+        return find(n->parent);
     }
 
 }
@@ -23,26 +24,37 @@ Node find(Node* n) {
 
 void myUnion(Node* x, Node* y) {
 
-    Node xLeader = find(&x);
-    Node yLeader = find(&y);
+    Node* xLeader = find(x);
+    Node* yLeader = find(y);
+    printf("xLeader elem: %d\n",xLeader->elem);
+    printf("xLeader parent: %p\n\n", xLeader->parent);
+    printf("yLeader elem: %d\n",yLeader->elem);
+    printf("yLeader parent: %p\n", yLeader->parent);
     if(xLeader != yLeader) {
         xLeader->parent = yLeader;
+        components--;
     }
 
 }
 
 int main(void) {
 
-    int piggyBanks,components;
+    int piggyBanks;
     scanf("%d",&piggyBanks);
     components = piggyBanks;
     std::vector<Node> graph = std::vector<Node>();
 
     for(int i=0;i<piggyBanks;i++) {
     
-        Node n = {i,&n};
+        Node n = {i, &n};
         graph.push_back(n);
-    
+       graph[i].parent = &graph[i]; 
+       printf("n's parent is: %p\n", n.parent);
+
+    }
+
+    for(int i=0; i<graph.size();i++) {
+        printf("i's parent and elem: %d %p %d\n",i,graph[i].parent,graph[i].elem);
     }
 
     for(int i=0;i<piggyBanks;i++) {
@@ -53,12 +65,13 @@ int main(void) {
        
         if(key != i) {
         
-            myUnion(graph[i], graph[key]);
-            components--;
+            myUnion(&graph[i], &graph[key]);
         
         }
 
     }
+
+    printf("Number of components: %d\n",components);
     
     return 0;
 

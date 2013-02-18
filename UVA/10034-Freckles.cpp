5b1f5497cdb1e48520a14b8aa66d5c6c40c8d5a6
dhu009 <dhu009@student.uib.no>
2013-02-18 15:21:34 +0100

    First draft UVA Freckles

---
+++ UVA/10034-Freckles.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <vector>
+
+int main(void) {
+
+    int cases;
+    scanf("%d\n",&cases);
+    for(int t=0;t<cases;t++) {
+    
+        int freckles;
+        scanf("%d",&freckles);
+        for(int i=0;i<freckles;i++) {
+        
+            float a,b;
+            scanf("%f %f",&a,&b);
+            std::pair<float,float> coords (a,b);
+        
+        }
+    
+    }
+
+    return 0;
+
+}

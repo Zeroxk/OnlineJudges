bbaae720cda73869c1606674a0a35ffdc08d0d0e
dhu009 <dhu009@student.uib.no>
2013-03-13 15:49:13 +0100

    Started on UVA Tourist Guide

---
+++ UVA/10099-Tourist_Guide.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <vector>
+
+int main(void) {
+
+    int cases;
+    scanf("%d",&cases);
+    while(cases > 0) {
+    
+        int cities,roads;
+        scanf("%d %d", &cities, &roads);
+
+        for(int r=0; r<roads; r++) {
+        
+            int city1,city2,passengers;
+            scanf("%d %d %d",&city1,&city2,&passengers);
+
+        }
+
+        cases--;
+    }
+
+    return 0;
+}

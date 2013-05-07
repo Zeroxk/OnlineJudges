6d5077c0fb3105d26dab13852c46d8550a0c06a3
Zeroxk <dhu009@student.uib.no>
2013-05-07 02:33:25 +0200

    Fire station added

---
+++ UVA/10278-Fire_Station.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <vector>
+
+int main(void) {
+    int cases;
+    scanf("%d\n", &cases);
+
+    while(cases > 0) {
+        int fireStations, intersections;
+        scanf("%d %d", &fireStations, &intersections);
+
+        for(int i=0; i<fireStations; i++) {
+            int fsNode;
+            scanf("%d", &fsNode);
+        }
+
+        for(int i=0; i<intersections; i++) {
+            int a,b,len;
+            scanf("%d %d %d", &a, &b, &len);
+        }
+
+        cases--;
+    }
+
+    return 0;
+}

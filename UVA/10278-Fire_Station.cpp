34282c097629154de7b539a0ebe3a2e4f529d7e3
dhu009 <zeroxk123@gmail.com>
2013-05-07 12:45:03 +0200

    Upd FireStation

--- UVA/10278-Fire_Station.cpp
+++ UVA/10278-Fire_Station.cpp
@@ -1,6 +1,11 @@
 #include <cstdio>
 #include <vector>
 
+struct Edge {
+    int a,b;
+    int len;
+}
+
 int main(void) {
     int cases;
     scanf("%d\n", &cases);
@@ -9,14 +14,22 @@ int main(void) {
         int fireStations, intersections;
         scanf("%d %d", &fireStations, &intersections);
 
+        std::vector<int> fs = std::vector<int>();
+
         for(int i=0; i<fireStations; i++) {
             int fsNode;
             scanf("%d", &fsNode);
+            fsNode--;
+            fs.push_back(fsNode);
         }
+
+        std::vector<Edge> edgeList = std::vector<Edge>();
 
         for(int i=0; i<intersections; i++) {
             int a,b,len;
             scanf("%d %d %d", &a, &b, &len);
+            Edge e = {a,b,len};
+            edgeList.push_back(e);
         }
 
         cases--;

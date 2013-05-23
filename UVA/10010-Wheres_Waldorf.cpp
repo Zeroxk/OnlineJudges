971ec8fda0546bfd39e495fca22e90fcdcabb5cd
Zeroxk <dhu009@student.uib.no>
2013-05-23 05:05:20 +0200

    WIP Where's Waldorf

---
+++ UVA/10010-Wheres_Waldorf.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+
+    int cases;
+    cin >> cases;
+
+    while(cases--) {
+        int rows, cols;
+        cin >> rows >> cols;
+        
+        while(rows--) {
+           string str = "";
+           getline(cin, str);
+        }
+    }
+
+    return 0;
+}

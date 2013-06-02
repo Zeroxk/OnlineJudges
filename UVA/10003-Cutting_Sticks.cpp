587074498651684a8e85c0b858761d3ee97ac277
Zeroxk <dhu009@student.uib.no>
2013-06-02 18:51:27 +0200

    Accepted Cutting Sticks

--- UVA/10003-Cutting_Sticks.cpp
+++ UVA/10003-Cutting_Sticks.cpp
@@ -1,9 +1,25 @@
 #include <cstdio>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
-vector<int> cuts;
+int cuts[51];
+int memo[51][51];
+
+int minCut(int start, int end) {
+    if(start+1 == end) return 0;
+    if(memo[start][end] != -1) return memo[start][end];
+
+    int minimum = INT_MAX;
+
+    for(int i=start+1; i<end; i++) {
+        minimum = min(minimum, minCut(start,i)+minCut(i,end)+cuts[end]-cuts[start]);
+    }
+
+    memo[start][end] = minimum;
+    return minimum;
+}
 
 int main(void) {
     
@@ -16,14 +32,24 @@ int main(void) {
         int n;
         scanf("%d", &n);
 
-        while(n--) {
+        for(int i=0; i<51; i++) {
+            for(int j=0; j<51; j++) {
+                memo[i][j] = -1;
+            }
+        }
+
+        for(int i=1; i<=n; i++) {
             int cut;
             scanf("%d", &cut);
-            cuts.push_back(cut);
-        }
+            cuts[i] = cut;
+        } 
         
         cuts[0] = 0;
-        cuts.back = length;
+        cuts[n+1] = length;
+
+        int ans = minCut(0, n+1);
+
+        printf("The minimum cutting is %d.\n", ans);
 
     }
 

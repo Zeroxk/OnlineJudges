53b18c473040ac2a7b66b20378c57bdd3141f5b9
dhu009 <dhu009@student.uib.no>
2013-06-06 18:11:21 +0200

    WIP Offices

--- main.edu.pl/XIV_OI-Offices.cpp
+++ main.edu.pl/XIV_OI-Offices.cpp
@@ -1,30 +1,38 @@
 #include <cstdio>
 #include <vector>
+#include <cstring>
+#include <algorithm>
 
-const int MAXNODES = 100000;
+const int MAXNODES = 100001;
+
 std::vector<int> officeSize;
 int components;
 int visited[MAXNODES];
 
-void dfs(std::vector<std:.vector<int> >& g, int node);
+void dfs(std::vector<std::vector<int> > &g, int node) {
 
-void makeInverse(std::vector<std::vector<int> >& g) {
+    visited[node] = 1;
+    for(int i=0;i<g[node].size();i++) {
+        
+        int isEdge = g[node][i];
+        if(isEdge == 1) {
+            if(visited[i] == -1) {
+                dfs(g,i);
+                officeSize[components]++;
+            }
+        }
+        
+
+    }
+
+}
+
+void makeInverse(std::vector<std::vector<int> > &g) {
 
     for(int i=0;i<g.size();i++) {
         for(int j=0;j<g[i].size();j++) {
         
-            for(int k=0;j<g.size();i++) {
-                bool found = false;
-                if(k != i && k == g[i][j]) {
-                    found = true;
-                }
-                
-                if(!found) {
-                    g[i].push_back(k);
-                    g[k].push_back(i);
-                }
-
-            }
+           g[i][j] = g[i][j] == 1 ? 0:1; 
 
         }
     
@@ -33,33 +41,16 @@ void makeInverse(std::vector<std::vector<int> >& g) {
 }
 
 
-int findComponents(std::vector<std::vector<int> >& g) {
+void findComponents(std::vector<std::vector<int> > &g) {
 
-    components = 0;
-    for(int i;i<g.size(); i++) {
-        
+    for(int i=0;i<g.size(); i++) {
             if(visited[i] == -1) {
+                //printf("Exploring %d\n", i+1); 
+                officeSize.push_back(1);
                 dfs(g, i);
-                officeSize.push_back(0);
                 components++;
             }
         
-    }
-
-    return 0;
-
-}
-
-void dfs(std::vector<std::vector<int> >& g, int node) {
-
-    for(int i=0;i<g[node].size();i++) {
-        
-        int neighbour = g[node][i];
-        if(visited[neighbour] == -1) {
-            dfs(g,neighbour);
-            officeSize[components]++;
-        }
-
     }
 
 }
@@ -71,22 +62,44 @@ int main(void) {
     std::vector<std::vector<int> > graph = std::vector<std::vector<int> >(nodes);
     officeSize = std::vector<int>();
 
+    for(int i=0; i<nodes; i++) {
+        graph[i] = std::vector<int>(nodes,0);
+    }
+
     for(int i=0;i<edges;i++) {
     
         int a,b;
-        scanf("%d %d\n",&a,&b);
-
+        scanf("%d %d",&a,&b);
         a--; b--;
-        graph[a].push_back(b);
-        graph[b].push_back(a);
+
+        graph[a][b] = 1;
+        graph[b][a] = 1;
 
     }
 
     makeInverse(graph);
-    int components = findComponents(graph);
+    //printf("Made inverse\n");
+    
+    /*for(int i=0; i<graph.size(); i++) {
+        printf("Node %d: ", i+1);
+        for(int j=0; j<graph[i].size(); j++) {
+            printf("%d ", graph[i][j]);
+        }
+        printf("\n");
+    }*/
+   
+    memset(visited, -1, sizeof(visited));
+    findComponents(graph);
 
-    printf("Number of components: %d\n",components);
-
+    printf("%d\n",components);
+    std::stable_sort(officeSize.begin(), officeSize.end());
+    for(int i=0; i<officeSize.size(); i++) {
+        if(i != officeSize.size()-1) {
+            printf("%d ", officeSize[i]);
+        }else {
+            printf("%d\n", officeSize[i]);
+        }
+    }
     return 0;
 
 }

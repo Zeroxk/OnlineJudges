9f626bb0d173386e6c7ac7dc643f62d2af5d573e
Zeroxk <dhu009@student.uib.no>
2013-06-07 04:51:25 +0200

    Accepted Guilds

---
+++ main.edu.pl/XVII_OI-Guilds.cpp
@@ -0,0 +1,68 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+struct Edge {
+    int a,b;
+};
+
+int main(void) {
+    int n,m;
+    scanf("%d %d", &n, &m);
+
+    vector<char> guilds (n);
+    vector<Edge> edges;
+    fill(guilds.begin(), guilds.end(), 'N');
+    for(int i=0; i<m; i++) {
+        int a,b;
+        scanf("%d %d", &a, &b);
+        a--; b--;
+        Edge e = {a,b};
+        edges.push_back(e);
+    }
+
+    for(int i=0; i<edges.size(); i++) {
+        Edge e = edges[i];
+        if(guilds[e.a] == 'N') {
+            if(guilds[e.b] == 'K') {
+                guilds[e.a] = 'S';
+            }else {
+                guilds[e.a] = 'K';
+            }
+        }
+
+        if(guilds[e.b] == 'N') {
+            if(guilds[e.a] == 'K') {
+                guilds[e.b] = 'S';
+            }else {
+                guilds[e.b] = 'K';
+            }
+        }
+    }
+
+    /*for(int i=0; i<guilds.size(); i++) {
+        printf("%c\n", guilds[i]);
+    }*/
+
+    bool nie = false;
+    for(int i=0; i<guilds.size(); i++) {
+        if(guilds[i] == 'N') {
+            nie = true;
+            break;
+        }
+    }
+
+    if(nie) {
+        printf("NIE\n");
+    }else {
+        printf("TAK\n");
+        for(int i=0; i<guilds.size(); i++) {
+            printf("%c\n", guilds[i]);
+        }
+    }  
+    
+    return 0;
+}

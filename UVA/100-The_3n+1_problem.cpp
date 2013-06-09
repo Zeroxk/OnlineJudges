0700d662067b9647c6b34aa9d0d028a3f990732e
Zeroxk <dhu009@student.uib.no>
2013-06-09 02:56:05 +0200

    3n+1 refactored w bitwise operations

--- UVA/100-The_3n+1_problem.cpp
+++ UVA/100-The_3n+1_problem.cpp
@@ -3,8 +3,12 @@
 
 using namespace std;
 
+const unsigned int SIZE = 1000000;
+
 int main(void) {
     unsigned int a,b;
+    unsigned int memo[SIZE];
+    //printf("%u\n", sizeof(memo));
     while(scanf("%u %u", &a, &b) != EOF) {
         unsigned int maxCycles = 0;
         
@@ -15,14 +19,26 @@ int main(void) {
         }
 
         for(unsigned int i=a; i<=b; i++) {
-           unsigned int tmp =1;
-           unsigned int n = i;
-           while(n != 1) {
-               tmp++;
-               n = n % 2 == 1 ? (3*n)+1 : n/2; 
-           }
+            unsigned int tmp =1;
+            if(memo[i-1] == 0) {
+                //printf("Undiscovered cycle length\n");
 
-           maxCycles = max(maxCycles, tmp);
+                unsigned int n = i;
+
+                while(n != 1) {
+                    //printf("n is %u\n", n);
+                    tmp ++;
+                    
+                    n = n & 1 == 1 ? (3*n)+1 : n >> 1;
+                    //n = n % 2 == 1 ? (3*n)+1 : n >> 1; 
+                }
+                memo[i-1] = tmp;
+            }else {
+                //printf("Already found\n");
+                tmp = memo[i-1];
+            }
+
+            maxCycles = max(maxCycles, tmp);
         }
         
         if(!swapped) {

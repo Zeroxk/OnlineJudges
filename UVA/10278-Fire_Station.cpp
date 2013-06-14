1a1dbd745ca7f0406b3ebde1d56c239af7c82573
Zeroxk <dhu009@student.uib.no>
2013-06-14 03:09:40 +0200

    WIP Fire Station

--- UVA/10278-Fire_Station.cpp
+++ UVA/10278-Fire_Station.cpp
@@ -3,75 +3,43 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <climits>
 
-struct Edge {
-    int a,b;
-    int len;
-};
+using namespace std;
 
-int dist[501][501];
-int intersections;
-std::vector<int> fs;
+const int INF = 1000000;
 
-int solve1() {
-    int node = -1;
-    int minDist = 1000000;
-    for(int i=0; i<intersections; i++) {
-        int max = 0;
-        for(int j=0; j<intersections; j++) {
-            if(dist[i][j] > max) {
-                max = dist[i][j];
-            }
-        }
-        if(max < minDist) {
-            minDist = max;
-            node = i;
-        }
-    }
-    
-    return node;
-}
-
-int solveN() {
-    int node = -1;
-    int maxDist = -1;
-    for(int i=0; i<intersections; i++) {
-        if(fs[i] == 1) continue;
-        int min = 1000000;
-        for(int j=0; j<fs.size(); j++) {
-            if(fs[j] == 0) continue;
-            if(dist[i][j] < min) {
-                min = dist[i][j];
-            }
-        }
-        //printf("i : %d, Min: %d, Max: %d\n", i+1, min, maxDist);
-        if(min > maxDist) {
-            maxDist = min;
-            node = i;
-        } 
-    }
-    
-    return node;
-}
+int dist[510][510];
 
 int main(void) {
     int cases;
     scanf("%d\n", &cases);
 
-    while(cases > 0) {
-        int fireStations;
+    while(cases--) {
+        int fireStations, intersections;
         scanf("%d %d\n", &fireStations, &intersections);
         //printf("fs: %d, is: %d\n", fireStations, intersections);
         
-        
-            fs = std::vector<int>(intersections);
-        
-
+        vector<int>fs (intersections);
         for(int i=0; i<fireStations; i++) {
             int fsNode;
             scanf("%d\n", &fsNode);
             fsNode--;
             fs[fsNode] = 1;
+        }
+
+        for(int i=0; i<intersections; i++) {
+            for(int j=0; j<intersections; j++) {
+                dist[i][j] = INF;
+            }
+            dist[i][i] = 0;
+        }
+
+        int a,b,len;
+        while(scanf("%d %d %d", &a, &b, &len) == 3) {
+            a--; b--;
+            dist[a][b] = len;
+            dist[b][a] = len;
         }
         
         /*printf("Firestations: ");
@@ -80,53 +48,9 @@ int main(void) {
         }
         printf("\n");*/
 
-        std::vector<Edge> edgeList = std::vector<Edge>();
-
-        std::string line;
-        while(getline(std::cin, line)) {
-//printf("I'm in\n");
-            if(line == "" || std::cin.eof()) break;
-
-            int vals[3];
-            int index=0;
-
-            for(int i=0; i<line.length(); i++) {
-                std::string str = "";
-                while(line[i] != ' ') {
-                    str += line[i];
-                    i++;
-                }
-                vals[index++] = std::atoi(str.c_str());
-
-            }
-            vals[0]--;
-            vals[1]--;
-            Edge e = {vals[0], vals[1], vals[2]};
-            //printf("%d %d %d\n", e.a, e.b, e.len);
-            edgeList.push_back(e);
-            line = "";
-        }
-
         //Start floyd-warshall
         //printf("Starting FW, #%d\n", intersections);
-        for(int i=0; i<intersections; i++) {
-            for(int j=0; j<intersections; j++) {
-                dist[i][j] = 1000000;
-            }
-        }
-
-        //printf("Init dist[i][i]\n");
-        for(int i=0; i<intersections; i++) {
-            dist[i][i] = 0;
-        }
-
-        //printf("Putting edge weights in dist array\n");
-        for(int i=0; i<edgeList.size(); i++) {
-            Edge e = edgeList[i];
-            dist[e.a][e.b] = e.len;
-            dist[e.b][e.a] = e.len;
-        }
-
+        
         //printf("Compare dist\n");
         for(int k=0; k<intersections; k++) {
             for(int i=0; i<intersections; i++) {
@@ -142,37 +66,17 @@ int main(void) {
 
         //printf("FW done\n");
 
-        /*for(int i=0; i<intersections; i++) {
+        for(int i=0; i<intersections; i++) {
             printf("Paths for node %d: ", i+1);
             for(int j=0; j<intersections; j++) {
                 printf("%d ", dist[i][j]);
             }
             printf("\n");
-        }*/
-
-        int putFs = -1;
-        if(fireStations == 0) {
-            //printf("Solving for no FS\n");
-            putFs = solve1();
-        }else {
-            //printf("Solving for 1 or more FS\n");
-            putFs = solveN();
         }
-        putFs++;
 
-        //printf("Put fs here: %d", putFs);
-        printf("%d\n", putFs);
-        for(int i=0; i<intersections; i++) {
-            for(int j=0; j<intersections; j++) {
-                dist[i][j] = 0;
-            }
-        }
-        
-        intersections = 0;
-        edgeList.clear();
-        fs.clear();
-        
-        cases--;
+
+
+                
         if(cases > 0) {
             printf("\n");
         }
